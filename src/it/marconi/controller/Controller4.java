33d6528957236fab458a54543f46f5887012d28e
ptom <ptom@imacdimaurizio.lan>
2016-01-07 00:43:15 +0100

    versione funzionante properies e aggiunto Controller4

---
+++ src/it/marconi/controller/Controller4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.marconi.controller;
+
+/**
+ *
+ * @author ptom
+ */
+public class Controller4 implements Controller {
+
+    /**
+     *
+     */
+    public Controller4() {
+    }
+    
+    @Override
+    public String handleRequest (String st){
+        return "Controller4";
+    }
+}
+
